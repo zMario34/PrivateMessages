237dbb34ebfdac742a78d9c84d08ddeca2a4d644
Mario <mariodedo08@gmail.com>
2023-03-25 17:28:20 +0100

    Renamed modules and removed getConsoleSender method

---
+++ privatemessages-common/src/main/java/tech/zmario/privatemessages/common/factory/SenderFactory.java
@@ -0,0 +1,31 @@
+package tech.zmario.privatemessages.common.factory;
+
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import tech.zmario.privatemessages.common.factory.user.Sender;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SenderFactory<T> {
+
+    Optional<Sender> getSender(String name);
+    Optional<Sender> getSender(UUID uuid);
+
+    Sender getSender(T player);
+
+    String getName(T player);
+
+    String getServerName(Sender sender);
+
+    void sendMessage(T player, Component component);
+
+    void playSound(T sender, Sound sound);
+
+    boolean hasPermission(T player, String permission);
+
+    boolean isPlayerOnline(String name);
+
+    boolean isConsole(Sender sender);
+
+}
---
+++ privatemessages-velocity/src/main/java/tech/zmario/privatemessages/velocity/factory/VelocitySenderFactory.java
@@ -0,0 +1,91 @@
+package tech.zmario.privatemessages.velocity.factory;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.ConsoleCommandSource;
+import com.velocitypowered.api.proxy.Player;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import tech.zmario.privatemessages.common.factory.SenderFactory;
+import tech.zmario.privatemessages.common.factory.user.Sender;
+import tech.zmario.privatemessages.common.factory.user.impl.BaseSender;
+import tech.zmario.privatemessages.velocity.VelocityPlugin;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class VelocitySenderFactory implements SenderFactory<CommandSource> {
+
+    private final VelocityPlugin plugin;
+
+    @Override
+    public Optional<Sender> getSender(String name) {
+        return getProxyUser(name).map(this::getSender);
+    }
+
+    @Override
+    public Optional<Sender> getSender(UUID uuid) {
+        return plugin.getBootstrap().getProxyServer().getPlayer(uuid).map(this::getSender);
+    }
+
+    @Override
+    public Sender getSender(CommandSource sender) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            return new BaseSender<>(player.getUniqueId(), player, this);
+        }
+
+        return new BaseSender<>(UUID.randomUUID(), sender, this);
+    }
+
+    private Optional<CommandSource> getProxyUser(String name) {
+        Optional<Player> player = plugin.getBootstrap().getProxyServer().getPlayer(name);
+
+        if (player.isPresent()) {
+            return player.map(CommandSource.class::cast);
+        }
+        return player.map(CommandSource.class::cast).or(Optional::empty);
+    }
+
+    @Override
+    public String getName(CommandSource sender) {
+        if (sender instanceof Player) {
+            return ((Player) sender).getUsername();
+        }
+
+        return "Console";
+    }
+
+    @Override
+    public String getServerName(Sender sender) {
+        return plugin.getBootstrap().getProxyServer().getServer(sender.getName())
+                .map(server -> server.getServerInfo().getName())
+                .orElse("Unknown");
+    }
+
+    @Override
+    public void sendMessage(CommandSource sender, Component component) {
+        sender.sendMessage(component);
+    }
+
+    @Override
+    public void playSound(CommandSource sender, Sound sound) {
+        sender.playSound(sound);
+    }
+
+    @Override
+    public boolean hasPermission(CommandSource sender, String permission) {
+        return sender.hasPermission(permission);
+    }
+
+    @Override
+    public boolean isPlayerOnline(String name) {
+        return plugin.getBootstrap().getProxyServer().getPlayer(name).isPresent();
+    }
+
+    @Override
+    public boolean isConsole(Sender sender) {
+        return getProxyUser(sender.getName()).map(source -> source instanceof ConsoleCommandSource).orElse(false);
+    }
+}
